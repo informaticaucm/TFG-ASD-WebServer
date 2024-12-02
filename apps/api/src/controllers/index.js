51910ce4e172f194461b0d70e400d318543ff96e
Michael Paredes Sanchez <michaelsteven2000@hotmail.com>
2024-12-02 20:52:37 +0100

    Desacople terminado

--- apps/api/src/controllers/index.js
+++ apps/api/src/controllers/index.js
@@ -10,3 +10,17 @@ export * from './qrs.js'; //done
 export * from './recurrencias.js'; //done
 //export * from './seguimiento.js';
 export * from './usuarios.js'; //done
+
+/*
+export * from './actividadesController.js'; //done
+export * from './asignaturasController.js';//done
+export * from './asistenciaController.js'; //done
+export * from './clasesController.js'; //done
+export * from './dispositivosController.js'; //done
+export * from './espaciosController.js'; //done
+export * from './excepcionesController.js'; //done
+export * from './gruposController.js'; //done
+export * from './qrsController.js'; //done
+export * from './recurrenciasController.js'; //done
+export * from './usuariosController.js'; //done
+*/
