4b623e31eeb8d95691d54f113278ad69f0026364
Michael Paredes Sanchez <michaelp@ucm.es>
2025-02-23 13:45:54 +0100

    Eliminacion de mas transacciones en Controllers

--- apps/api/src_copy/controllers/gruposController.js
+++ apps/api/src_copy/controllers/gruposController.js
@@ -1,8 +1,44 @@
 // controllers/gruposController.js
+
 import { getGrupoById, getGrupoByCursoLetra } from '../services/grupos.js';
 import { AppError, errorValidacion, notExpectedError } from '../utils/errors.js';
 
 export function gruposControllerFactory(db) {
+    return {
+        async getGrupoById(req, res, next) {
+            const idGrupo = Number(req.params.idGrupo);
+            if (!Number.isInteger(idGrupo)) {
+                return next(errorValidacion('Id suministrado no válido'));
+            }
+
+            try {
+                const grupo = await getGrupoById(db, idGrupo); // Llama al servicio
+                res.status(200).json(grupo);
+            } catch (error) {
+                next(error instanceof AppError ? error : notExpectedError({ cause: error }));
+            }
+        },
+
+        async getGrupoByCursoLetra(req, res, next) {
+            const curso = Number(req.query.curso); // Ahora en `query` en lugar de `body`
+            const letra = req.query.letra;
+
+            if (!Number.isInteger(curso) || typeof letra !== 'string') {
+                return next(errorValidacion('Datos suministrados no válidos'));
+            }
+
+            try {
+                const grupo = await getGrupoByCursoLetra(db, curso, letra); // Llama al servicio
+                res.status(200).json(grupo);
+            } catch (error) {
+                next(error instanceof AppError ? error : notExpectedError({ cause: error }));
+            }
+        }
+    };
+}
+
+
+/*export function gruposControllerFactory(db) {
     return {
         async getGrupoById(req, res, next) {
             const idGrupo = Number(req.params.idGrupo);
@@ -42,4 +78,4 @@ export function gruposControllerFactory(db) {
             }
         }
     };
-}
+}*/
