2c0594761d8b3a856d3ae817e30a9cfce4538575
Diego Montero <diemon01@ucm.es>
2025-02-23 14:20:15 +0100

    Tests asignaturas

--- apps/api/src_copy/services/asignatura.js
+++ apps/api/src_copy/services/asignatura.js
@@ -1,12 +1,17 @@
+import { apiLogger } from '../../../../packages/logger/src/logger.js';  // Asumimos que tienes un logger configurado
+import { AppError, notFoundError } from '../errors/errors.js';
+
 export async function getAsignaturaById(db, idAsignatura) {
     const asignatura = await db.sequelize.models.Asignatura.findOne({
         attributes: ['nombre', 'siglas', 'departamento', 'periodo'],
         where: { id: idAsignatura }
     });
-
+    console.log(asignatura)
     if (!asignatura) {
-        throw { status: 404, message: 'Asignatura no encontrada' };
+        console.log("ERROR")
+        throw notFoundError('Asignatura no encontrada');
+        //{ status: 404, message: 'Asignatura no encontrada' }
     }
-
+    console.log("TODO OK")
     return asignatura;
 }
