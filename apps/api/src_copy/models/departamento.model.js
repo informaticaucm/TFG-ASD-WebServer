511a42b048a79daa78e6db828bf35def4d182058
Diego Montero <diemon01@ucm.es>
2025-04-16 21:25:24 +0200

    comentado

--- apps/api/src_copy/models/departamento.model.js
+++ apps/api/src_copy/models/departamento.model.js
@@ -24,9 +24,9 @@ export function model(sequelize) {
     });
 
     Departamento.associate = function (models) {
-        models.Departamento.hasMany(models.Docente, { as: 'Miembros', foreignKey: { name: 'docente_id', allowNull: false }}); //Un departamento tiene varios docentes
+        /*models.Departamento.hasMany(models.Docente, { as: 'Miembros', foreignKey: { name: 'docente_id', allowNull: false }}); //Un departamento tiene varios docentes
         models.Departamento.belongsToMany(models.Docente, { as: 'pertenece', through: { model: models.Join_Departamento_Docentes, foreignKey: 'departamento_id', allowNull: false }, foreignKey: 'departamento_id' });
-
+*/
     }; 
 
     return Departamento;
